25afcdda40c5a5c313d75b5bcf3d72dbc846c9f4
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-07-15 12:08:54 -0400

    - starting user login system

--- app/web/index.php
+++ app/web/index.php
@@ -5,24 +5,52 @@ require_once __DIR__. "/../vendor/autoload.php";
 use Ihsw\TwitterClone\Application;
 use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\HttpFoundation\RedirectResponse;
+use Silex\Provider\SecurityServiceProvider,
+    Silex\Provider\SessionServiceProvider,
+    Silex\Provider\TwigServiceProvider,
+    Silex\Provider\UrlGeneratorServiceProvider;
 
-$twig = new \Twig_Environment(new \Twig_Loader_Filesystem("../templates"), [
-    // "cache" => "../cache"
-    "cache" => false
-]);
+// starting it up
 $app = new Application();
-
 $app["debug"] = true;
 
-$app->before(function (Request $request, Application $app) use($twig) {
-    $app["twig"] = $twig;
+// service setup
+$twigCache = "../cache";
+$twigCache = false;
+$app->register(new SecurityServiceProvider())
+    ->register(new SessionServiceProvider())
+    ->register(new TwigServiceProvider(), ["twig.path" => "../templates", "twig.options" => ["cache" => $twigCache]])
+    ->register(new UrlGeneratorServiceProvider());
 
-    if ($request->getRequestUri() !== "/login") {
-        return new RedirectResponse("/login");
-    }
+// firewall definitions
+$app["security.firewalls"] = [
+    "login" => [
+        "pattern" => "^/login$"
+    ],
+    "secured" => [
+        "pattern" => "^.*$",
+        "form" => ["login_path" => "/login", "check_path" => "/login_check"],
+        "logout" => ["logout_path" => "/logout"],
+        "users" => [
+            // raw password is foo
+            "admin" => [
+                "ROLE_ADMIN",
+                "5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=="
+            ],
+        ]
+    ],
+];
+
+// route setup
+$app->get("/", function (Application $app) {
+    return sprintf("Hello, %s!", $app["security.token_storage"]->getToken()->getUser()->getUsername());
 });
-$app->get("/", function () { return "Hello, world!"; });
-$app->get("/login", function (Application $app) {
-    return $app["twig"]->render("login.html.twig");
+$app->get("/login", function (Application $app, Request $request) {
+    return $app["twig"]->render("login.html.twig", [
+        "error" => $app["security.last_error"]($request),
+        "last_username" => $app["session"]->get("_security.last_username"),
+    ]);
 });
+
+// running it out
 $app->run();
